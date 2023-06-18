5f85d1c24533c24399f88b74ca3e051f63389f50
satish chaudhary <sung20700@gmail.com>
2023-06-18 17:25:08 +0545

    javascript varaible

---
+++ extra class/javascript/variables.js
@@ -0,0 +1,17 @@
+var a =1
+let b=2
+const c =3
+
+console.table({a,b,c})
+
+let arr = ["satish", {
+    "name":"satish",
+},{
+    "name":"dipesh",
+},{
+    "name":"nishan",
+},{
+    "Values":"ram",
+},
+]
+console.table(arr)
